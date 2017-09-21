02df90dbad98c3c6b9c6e0f0de93cf0c516464da
Andre <elij.richards22@gmail.com>
2017-09-21 05:40:57 -0400

    extracting data for products from db

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,42 +1,27 @@
 const {Apparel, Electronics, Homegoods} = require('../models/Inventory')
 module.exports = app => {
     app.get('/inventory/apparel', (req, res) => {
-        Apparel
-            .findOne({})
-            .then(items => {
+        Apparel.find().then(items => {
                 res.json(items)
-            })
-            .catch(err => {
+            }).catch(err => {
                 console.error(err)
-                res
-                    .status(500)
-                    .json({message: 'Internal server error'})
+                res.status(500).json({message: 'Internal server error'})
             })
     }),
     app.get('/inventory/electronics', (req, res) => {
-        Electronics
-            .findOne({})
-            .then(items => {
+        Electronics.find().then(items => {
                 res.json(items)
-            })
-            .catch(err => {
+            }).catch(err => {
                 console.error(err)
-                res
-                    .status(500)
-                    .json({message: 'Internal server error'})
+                res.status(500).json({message: 'Internal server error'})
             })
     }),
     app.get('/inventory/homegoods', (req, res) => {
-        Homegoods
-            .findOne({})
-            .then(items => {
+        Homegoods.find().then(items => {
                 res.json(items)
-            })
-            .catch(err => {
+            }).catch(err => {
                 console.error(err)
-                res
-                    .status(500)
-                    .json({message: 'Internal server error'})
+                res.status(500).json({message: 'Internal server error'})
             })
     })
 }
