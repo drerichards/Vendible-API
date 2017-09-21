5ec9bbbe60a8a35e942fa0806a26b8fb5bfda09f
Andre Richardson <g.andrerichards@gmail.com>
2017-09-21 05:37:08 -0400

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    googleClientID: '777502287478-q985306nqk8hpvihor13toedrie0sea1.apps.googleusercontent.com',
-    googleClientSecret: 'fvgBfxlnA-DNw0cpe9q7hglZ',
-    mongoURI: 'mongodb://andredev:hyetsb@ds129394.mlab.com:29394/vendible-dev',
-    cookieKey: 'p248oweryGEHWFJDK55o2i4tyhwefI3RUEWFHoqufahsczxlougrp2we',
-    stripePublishableKey: 'pk_test_72k7afMyvAZApegkYfgmOPMp',
-    stripeSecretKey: 'sk_test_TBTasApolFZT7t1lAHi0S2tB'
-}
