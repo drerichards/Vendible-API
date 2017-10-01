684a7cc3dcf0bdd259b2dbb0cfe7865412cbaa04
Andre <elij.richards22@gmail.com>
2017-10-01 18:30:29 -0400

    auth route update

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -13,7 +13,6 @@ class Payment extends Component {
                 name='Vendible'
                 description={description}
                 amount={payTotal}
-                token={token => this.props.handleToken(token, cartTotal)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}>
                 <button className="btn btn-info" style={{'fontSize': '1em'}}>Checkout</button>
             </StripeCheckout>
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const express = require('express'),
 require('./models/GoogleUser')
 require('./services/passport')
 
-
+app.use(cors())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
