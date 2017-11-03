3ee135295be6e90ca700d2d35985a3609839ad92
Andre <elij.richards22@gmail.com>
2017-11-02 20:23:48 -0400

    redeploy

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ module.exports = app => {
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']
     }))
-    app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/l' }), (req, res) => {
+    app.get('https://stormy-fortress-32507.herokuapp.com/auth/google/callback', passport.authenticate('google', { failureRedirect: '/l' }), (req, res) => {
       res.redirect('/')
     })
     app.get('/api/logout', (req, res) => {
