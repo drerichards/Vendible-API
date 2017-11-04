315d8ee3bbaa0f55c9608f7a0f9318122dc90ac5
Andre <elij.richards22@gmail.com>
2017-11-04 16:54:39 -0400

    test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ module.exports = app => {
     }))
     app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/l' }), (req, res) => {
         // res.redirect('https://vendible.netlify.com')
-        res.redirect(`'https://vendible.netlify.com/post=${req.user.first_name}`)
+        res.redirect(`/post=${req.user.first_name}`)
         // res.redirect('/')
         // console.log(req.user)
         // res.send( req.user)
