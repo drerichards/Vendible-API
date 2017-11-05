5fbec71f23d59c9c8ee34e23e62e7850d14ba268
Raymond Smith <raysmithdev@gmail.com>
2017-11-05 10:11:23 -0800

    1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,7 @@ module.exports = app => {
         scope: ['profile', 'email']
     }))
     app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/l', session: true}), (req, res) => {
-        res.redirect(`https://dry-oasis-35581.herokuapp.com/?${req.user}/`)
+        res.redirect(`https://dry-oasis-35581.herokuapp.com/?${req.user`)
     })
     app.get('/api/logout', (req, res) => {
         req.logout()
