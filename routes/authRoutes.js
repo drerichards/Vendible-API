8c74a745ce253151f89f792017cf52a6e6f3cc13
Andre <elij.richards22@gmail.com>
2017-11-06 01:21:08 -0500

    refactor

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,13 +5,10 @@ module.exports = app => {
         scope: ['profile', 'email']
     }))
     app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/l', session: false}), (req, res) => {
-        res.redirect(`https://salty-sands-12333.herokuapp.com/${req.user._id}/${req.user.email}/test`) 
+        res.redirect(`https://salty-sands-12333.herokuapp.com/${req.user._id}/${req.user.email}`) 
     })
     app.get('/api/logout', (req, res) => {
         req.logout()
         res.redirect('https://salty-sands-12333.herokuapp.com')
-    })
-    app.get('/api/current_user',  (req, res) => {
-        res.json({msg: 'yup', user: req.session, t: '3'})
     })
 }
